97ecc169ab55896909c3d33efa55bee6fe44fb5f
Steve Manuel <steve@dylib.so>
2024-03-01 13:21:03 -0700

    fix: update import paths to use reexported instrument macro

--- src/guest/rust/src/main.rs
+++ src/guest/rust/src/main.rs
@@ -1,6 +1,5 @@
 #![allow(dead_code)]
 use observe_api::*;
-use observe_instrument::instrument;
 use serde::Deserialize;
 
 const PRODUCT_JSON: &str = r#"{"id":1,"title":"iPhone 9","description":"An apple mobile which is nothing like apple","price":549,"discountPercentage":12.96,"rating":4.69,"stock":94,"brand":"Apple","category":"smartphones","thumbnail":"https://cdn.dummyjson.com/product-images/1/thumbnail.jpg","images":["https://cdn.dummyjson.com/product-images/1/1.jpg","https://cdn.dummyjson.com/product-images/1/2.jpg","https://cdn.dummyjson.com/product-images/1/3.jpg","https://cdn.dummyjson.com/product-images/1/4.jpg","https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"]}"#;
